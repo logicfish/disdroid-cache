dc6559c6901eb99d5d35f6818395e0023e654d4c
DisDroid IQI <disdroid.iqi@gmail.com>
2022-08-06 03:41:55 +0100

    theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,20 +2,20 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     {
-      resolve: "gatsby-theme-amaranth",
+      resolve: "gatsby-theme-disdronic",
       options: {
         website: {
-              title: "DisDroid/IQI : Purple Cache", // Homepage title
+              title: "DisDroid/IQI : Purple Cachei #iqsec", // Homepage title
               titleShort: "DisDroid:purple", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
               name: "DisDroid/Purple", // Website name used for homescreen (PWA) and SEO
-              description: "DisDroid/IQI Purple cache site.", // Website description used for RSS feeds/meta description tag
+              description: "DisDroid/IQI Purple #iqsec", // Website description used for RSS feeds/meta description tag
               language: "en", // Sets the global HTML lang attribute
               logoUrl: "/logos/logo-1024.png", // Logo used for SEO
               fbAppId: "1825356251115265", // FB Application ID for using app insights
               twitterName: "disdroidi", // Twitter handle of the website
               url: "https://disdroid.netlify.app", // Domain of your website without the pathPrefix
               rss: "/rss.xml", // Path to the RSS file
-              rssTitle: "DisDroid/IQI Purple cache site.", // Title of the RSS feed
+              rssTitle: "DisDroid/IQI Purple cache #iqsec", // Title of the RSS feed
 
               googleAnalyticsId: "UA-VALIDID", // GA tracking ID
               copyright: "© Copyright 2022 | DisDroid/IQI", // Copyright string for the footer of the website and RSS feed.
@@ -25,15 +25,15 @@ module.exports = {
         },
 	  // User configuration
         user: {
-    	      id: "DisDroid/IQI:purple", // Unique identifier of the user on the website. Used for OpenGraph SEO tags
+    	      id: "DisDroid/IQI:purple|#iqsec", // Unique identifier of the user on the website. Used for OpenGraph SEO tags
               firstName: "DisDroid/IQI", // Used for SEO
-              lastName: "Purple", // Used for SEO
+              lastName: "Purple|#iqsec", // Used for SEO
               twitterName: "disdroidi", // Twitter username used for SEO
               linkedIn: "your-linkedin", // Used for contact information
               github: "disdroid", // Used for contact information
               email: "iqi@disdroid.co.uk", // Used for contact information and displayed in the RSS feed
               location: "United Kingdom", // User location used for SEO
-              about: "DisDroid/IQI Purple Monitor", // User information used for the author section
+              about: "DisDroid/IQI Purple #iqsec", // User information used for the author section
               avatar: "https://i.pravatar.cc/300", // User avatar used for the author section
         },
 
